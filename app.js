9fa61c06045cc5144ea4b2a20849e65b333abcb0
Gabriel Faleiro Oliveira <149822445+Faleiro01@users.noreply.github.com>
2023-11-14 13:58:38 -0500

    First classes with JS
    Progam Logic & Flow

---
+++ app.js
@@ -0,0 +1,127 @@
+// let mystery = '';
+
+// if (mystery) {
+//     console.log("Truthy");
+// }
+// else {
+//     console.log('falsy')
+// }
+// let loggedInUser = null;
+
+// if (loggedInUser) {
+//     console.log('You are logged in!')
+// }
+// else {
+//     console.log("Please log in!")
+// }
+// 1 <= 4 && 'a' === 'a'
+// let password = 'Gabriel';
+
+// if (password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log("Valid Password!");
+// }
+// else {
+//     console.log("Invalid Password, Try again!");
+
+// }
+// let num = 15;
+
+// if (num >= 1 && num <= 10) {
+//     console.log("Number is between 1 and 10")
+
+// }
+// else {
+//     console.log('Please choose a number that is between 1 and 10');
+
+// }
+// let age = 76;
+// if (age < 6 || age >= 80) {
+//     console.log("You get in for free!");
+// }
+// else {
+//     console.log("That will be $10 please.")
+// }
+// let age = 30;
+// if (age >= 65) {
+//     console.log("You can come in for free!")
+// }
+// else if (age === 30) {
+//     console.log("You're almost there, pay half!")
+// }
+// else {
+//     console.log("You'll have to pay");
+// }
+// let color = 'green';
+// if (color === 'purple' || color === 'Lilac' || color === 'violet') {
+//     console.log("Great choice!")
+// }
+// else {
+//     console.log("Pick a better color next time!")
+// }
+// let loggedInUser = 'Gabriel';
+// if (!loggedInUser) {
+//     console.log("Get out of here!")
+// }
+// else {
+//     console.log("Welcome back!")
+// }
+// let flavor = 'watermelon';
+
+// if (flavor !== 'grape' && flavor !== 'cherry') {
+//     console.log('We dont have that flavor!');
+// }
+// if (!(flavor === 'grape' || flavor === 'cherry')) {
+//     console.log("Hello");
+// }
+// let day = 9;
+
+// if (day === 1) {
+//     console.log('Monday');
+// }
+// else if (day === 2) {
+//     console.log('Tuesday');
+// }
+// else if (day === 3) {
+//     console.log('Wednesday');
+// }
+// else if (day === 4) {
+//     console.log('Thursday');
+// }
+// else if (day === 5) {
+//     console.log('Friday');
+// }
+// else if (day === 6) {
+//     console.log('Saturday');
+// }
+// else if (day === 7) {
+//     console.log('Sunday');
+// }
+// else {
+//     console.log('Invalid');
+// }
+
+// switch (day) {
+//     case 1:
+//         console.log('Monday')
+//         break;
+//     case 2:
+//         console.log('Tuesday')
+//         break;
+//     case 3:
+//         console.log('Wednesday')
+//         break;
+//     case 4:
+//         console.log('Thursday')
+//         break;
+//     case 5:
+//         console.log('Friday')
+//         break;
+//     case 6:
+//         console.log('Saturday')
+//         break;
+//     case 7:
+//         console.log('Sunday')
+//         break;
+//     default:
+//         console.log('Invalid');
+// }
