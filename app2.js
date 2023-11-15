e2d2777b9240add5bc3bc56d9d6e5fdfe623fd1c
Gabriel Faleiro Oliveira <149822445+Faleiro01@users.noreply.github.com>
2023-11-15 12:44:11 -0500

    2nd excercise of JavaScript / my notes.

---
+++ app2.js
@@ -0,0 +1,54 @@
+let num = 2;
+
+if (num === 7) {
+    console.log('lucky');
+}
+else {
+    console.log('bad');
+}
+
+num === 7 ? console.log('lucky') : console.log('bad');
+
+let status = 'online';
+
+let colo;
+if (statu === 'offline') {
+    color = 'red';
+}
+else {
+    color = 'green';
+}
+let color = statu === 'offline' ? 'red' : 'green';
+
+let isLearning = true;
+if (isLearning) {
+    console.log("Keep it Up");
+}
+else {
+    console.log("Pretty sure you are learning...");
+}
+let firstVariable;
+let secondVariable = '';
+let thirdVariable = 1;
+let secretMessage = "Shh!";
+
+if (firstVariable) {
+    console.log("first");
+}
+else if (firstVariable || secondVariable) {
+    console.log("second");
+}
+else if (firstVariable || thirdVariable) {
+    console.log("third");
+}
+else {
+    console.log("fourth");
+}
+
+if (Math.random() >= 0.6) {
+    console.log("Over 0.5");
+}
+else {
+    console.log("Under 0.5")
+}
+
