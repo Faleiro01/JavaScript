37951f5ce334925e9972c4cdc6ff2e4b8aaad0b8
Gabriel Faleiro Oliveira <149822445+Faleiro01@users.noreply.github.com>
2023-11-21 19:54:26 -0500

    Add files via upload
    Uploading the excercises I did yesterday.

--- functions.js
+++ functions.js
@@ -136,4 +136,88 @@ function daysOfTheWeek(num) {
 }
 function lastElement(arr) {
     return arr[arr.length - 1];
+} function numberCompare(a, b) {
+    if (a === b) {
+        return 'Numbers are equal';
+    } else if (a > b) {
+        return 'First is greater';
+    }
+    return 'Second is greater';
+}
+
+function singleLetterCount(str1, letter) {
+    let finalCount = 0;
+    for (let i = 0; i < str1.length; i++) {
+        if (str1[i].toLowerCase() === letter.toLowerCase()) {
+            finalCount++;
+        }
+    }
+    return finalCount;
+}
+
+function multipleLetterCount(str) {
+    str = str.toLowerCase();
+    let finalObj = {};
+    for (let i = 0; i < str.length; i++) {
+        if (finalObj[str[i]] === undefined) {
+            finalObj[str[i]] = 1;
+        } else {
+            finalObj[str[i]]++;
+        }
+    }
+    return finalObj;
+}
+
+function arrayManipulation(arr, command, position, val) {
+    if (command === 'remove') {
+        if (position === 'end') {
+            return arr.pop();
+        }
+        return arr.shift();
+    }
+    else if (command === 'add') {
+        if (position === 'end') {
+            arr.push(val)
+            return arr;
+        }
+        arr.unshift(val)
+        return arr;
+    }
+}
+
+function isPalindrome(str) {
+    return str.toLowerCase().split('').reverse().join('') === str.toLowerCase();
+
+}
+function RPS() {
+
+    function determineComputer(num) {
+        if (num <= .33) return "rock";
+        else if (num <= .66) return "paper";
+        return "scissor";
+    }
+
+    let userChoice = prompt("Choose rock / paper or scissor").toLowerCase();
+    let computerChoice = determineComputer(Math.random());
+
+
+    let answers = ["rock", "paper", "scissor"];
+
+    if (!userChoice || answers.indexOf(userChoice) === -1) {
+        return "Please select a valid option";
+    }
+
+    if (userChoice === computerChoice) return "Tie!";
+
+    if (userChoice === "rock" && computerChoice === "paper") {
+        return "You lose, computer picked " + computerChoice;
+    }
+    if (userChoice === "paper" && computerChoice === "scissor") {
+        return "You lose, computer picked " + computerChoice;
+    }
+    if (userChoice === "scissor" && computerChoice === "rock") {
+        return "You lose, computer picked " + computerChoice;
+    }
+
+    return "You win! Computer picked " + computerChoice;
 }
