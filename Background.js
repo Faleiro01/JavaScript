f944fa56664b8dad422294dbc854f082fc285304
Gabriel Faleiro Oliveira <149822445+Faleiro01@users.noreply.github.com>
2023-12-09 22:20:54 -0500

    Add files via upload

---
+++ Background.js
@@ -0,0 +1,7 @@
+document.addEventListener('mousemove', function (e) {
+    const r = Math.round(e.pageX * 255 / window.innerWidth);
+    const b = Math.round(e.pageY * 255 / window.innerHeight);
+    const color = `rgb(${r}, 0, ${b})`;
+    document.body.style.backgroundColor = color;
+
+})
