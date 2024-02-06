f8b77fce31c25922b7e6a2ade3d88ab20e334734
Gabriel Faleiro Oliveira <149822445+Faleiro01@users.noreply.github.com>
2024-02-06 13:08:16 -0500

    forEach/map/filter Excercise

--- app.js
+++ app.js
@@ -1,66 +1,121 @@
-const words = [
-	'immunoelectrophoretically',
-	'rotavator',
-	'tsktsk',
-	'psychophysicotherapeutics',
-	'squirrelled',
-	'crypt',
-	'uncopyrightable',
-	'cysts',
-	'pseudopseudohypoparathyroidism',
-	'unimaginatively'
-];
+const double = [1,2,3,4,5,6];
 
-const containsVowel = function(word) {
-	for (let char of word) {
-		if (isVowel(char)) return true;
-	}
-	return false;
-};
-
-const isVowel = function(char) {
-	return 'aeiou'.indexOf(char) !== -1;
-};
-
-const longWords = words.filter(function(word) {
-	return word.length >= 20;
-});
-
-const cOrUWords = words.filter(function(w) {
-	return w[0] === 'u' || w[0] === 'c';
-});
-
-const containVowels = words.filter(containsVowel);
-
-const noVowels = words.filter(function(word) {
-	return !containsVowel(word);
-});
-
-function extractCompletedTodos() {
-	const allCheckboxes = document.querySelectorAll('input[type="checkbox"]');
-	return Array.from(allCheckboxes)
-		.filter(function(box) {
-			return box.checked;
-		})
-		.map(function(checkbox) {
-			return checkbox.parentElement.innerText;
-		});
+const doubleValues = function(arr){
+    let doubles = [];
+    arr.forEach(function(val) {
+        doubles.push(val * 2);
+    });
+    return doubles;
 }
 
-function myFilter(arr, callback) {
-	const filteredArray = [];
-	for (let i = 0; i < arr.length; i++) {
-		if (callback(arr[i], i, arr)) {
-			filteredArray.push(arr[i]);
-		}
-	}
-	return filteredArray;
+const onlyEven = function(arr){
+    let doubles = [];
+    arr.forEach(function(val) {
+        if(val % 2 === 0){
+        doubles.push(val);
+        }
+    });
+    return doubles;
 }
+const names = ['colt', 'joseph', 'tim', 'test'];
 
-const shorties = myFilter(words, function(word) {
-	return word.length <= 10;
-});
+function showFirstAndLast(arr) {
+    let newArr = [];
+    arr.forEach(function(val) {
+      newArr.push(val[0] + val[val.length - 1]);
+    });
+    return newArr;
+  }
 
-const everyOtherWord = myFilter(words, function(word, i) {
-	return i % 2 === 0;
-});
+  function addKeyAndValue(arr, key, value) {
+    arr.forEach(function(val) {
+      val[key] = value;
+    });
+    return arr;
+  }
+
+  function vowelCount(str) {
+    let splitArr = str.split("");
+    let obj = {};
+    const vowels = "aeiou";
+  
+    splitArr.forEach(function(letter) {
+      let lowerCasedLetter = letter.toLowerCase()
+      if (vowels.indexOf(lowerCasedLetter) !== -1) {
+        if (obj[lowerCasedLetter]) {
+          obj[lowerCasedLetter]++;
+        } else {
+          obj[lowerCasedLetter] = 1;
+        }
+      }
+    });
+    return obj;
+  }
+//map
+  
+function doubleValuesWithMap(arr) {
+    return arr.map(function(val) {
+      return val * 2;
+    });
+  }
+  
+  function valTimesIndex(arr) {
+    return arr.map(function(val, idx) {
+      return val * idx;
+    });
+  }
+  
+  function extractKey(arr, key) {
+    return arr.map(function(val) {
+      return val[key];
+    });
+  }
+  
+  function extractFullName(arr) {
+    return arr.map(function(val) {
+      return val.first + " " + val.last;
+    });
+  }
+  
+  // filter
+  
+  function filterByValue(arr, key) {
+    return arr.filter(function(val) {
+      return val[key] !== undefined;
+    });
+  }
+  
+  function find(arr, searchValue) {
+    return arr.filter(function(val) {
+      return val === searchValue;
+    })[0];
+  }
+  
+  function findInObj(arr, key, searchValue) {
+    return arr.filter(function(val) {
+      return val[key] === searchValue;
+    })[0];
+  }
+  
+  function removeVowels(str) {
+    const vowels = "aeiou";
+    return str
+      .toLowerCase()
+      .split("")
+      .filter(function(val) {
+        return vowels.indexOf(val) === -1;
+      })
+      .join("");
+  }
+  
+  function doubleOddNumbers(arr) {
+    return arr
+      .filter(function(val) {
+        return val % 2 !== 0;
+      })
+      .map(function(val) {
+        return val * 2;
+      });
+    }
+  
+  
