2745422edb65c7453364d175da5caacb061d9bfc
Gabriel Faleiro Oliveira <149822445+Faleiro01@users.noreply.github.com>
2024-02-08 23:31:54 -0500

    Reduce

---
+++ exReduce.js
@@ -0,0 +1,40 @@
+const extractValue = function(array, key){
+   return array.reduce(function(accumulator, currentVal){
+        accumulator.push(currentVal[key]);
+        return accumulator;
+
+        
+    },[])
+}
+function vowelCount(str){
+    const vowels = "aeiou";
+    return str.split('').reduce(function(acc,next){
+        let lowerCased = next.toLowerCase()
+        if(vowels.indexOf(lowerCased) !== -1){
+            if(acc[lowerCased]){
+                acc[lowerCased]++;
+            } else {
+                acc[lowerCased] = 1;
+            }
+        }
+        return acc;
+    }, {});
+}
+
+function addKeyAndValue(arr, key, value){
+    return arr.reduce(function(acc,next,idx){
+        acc[idx][key] = value;
+        return acc;
+    },arr);
+}
+
+function partition(arr, cb){
+    return arr.reduce(function(acc,next){
+        if(cb(next)){
+            acc[0].push(next);
+        } else {
+            acc[1].push(next);
+        }
+        return acc;
+    }, [[],[]]);
+}
